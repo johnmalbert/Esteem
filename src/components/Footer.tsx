db00118cc1a776fd08ae7c6216bbc9e6f2237171
John Albert <john.m.albert@gmail.com>
2025-04-22 11:12:05 -0700

    updating address

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
             Instagram
           </a>
         </p>
-        <p className="mt-1">1201 Dexter Ave., Seattle, WA 98109</p>
+        <p className="mt-1">600 N 36th St. #209, Seattle, WA 98103</p>
         <a
           href="https://square.site/book/1NCFYYAC5EK9Y/ross-henry-seattle-wa"
           className="bg-[#2A65AB] text-white px-4 py-2 rounded-md inline-block mt-2 hover:bg-blue-600"
@@ -25,7 +25,7 @@ const Footer = () => {
       <div className="mt-6">
         <iframe
           title="Esteem Seattle"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2687.681685410202!2d-122.35100820000001!3d47.6517522!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54901500aac92b1f%3A0x66584f1d27d37534!2s600%20N%2036th%20St%20Suite%20209%2C%20Seattle%2C%20WA%2098103!5e0!3m2!1sen!2sus!4v1744322596834!5m2!1sen!2sus"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2687.681686892706!2d-122.35358312298354!3d47.651752171193976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54901500aacf0b6b%3A0xcd1cb20352af9ac4!2s600%20N%2036th%20St%2C%20Seattle%2C%20WA%2098103!5e0!3m2!1sen!2sus!4v1745345415365!5m2!1sen!2sus"
           width="100%"
           height="250"
           className="rounded-md border-0 w-full max-w-2xl mx-auto"
