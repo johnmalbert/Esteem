bb59d8bbbd946618b7e0e2159e277f0040b6185a
John Albert <john.m.albert@gmail.com>
2025-09-13 14:22:05 -0700

    Adding AppInsights

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,18 @@
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+
+const connectionString = import.meta.env.VITE_APPINSIGHTS_CONNECTION_STRING;
+
+if (connectionString) {
+  const appInsights = new ApplicationInsights({
+    config: {
+      connectionString: connectionString,
+      autoTrackPageVisitTime: true,
+    }
+  });
+  appInsights.loadAppInsights();
+  appInsights.trackPageView(); // Tracks initial page view
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
